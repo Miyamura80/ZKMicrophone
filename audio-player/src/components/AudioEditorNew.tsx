bd4f8909fbcf1c0b6ef25b72f48d1db5b384c312
lucasege <lucasege@cs.stanford.edu>
2023-07-23 01:23:51 +0200

    Parking

--- audio-player/src/components/AudioEditorNew.tsx
+++ audio-player/src/components/AudioEditorNew.tsx
@@ -1,20 +1,28 @@
+import axios from 'axios';
 import { useEffect, useRef, useState } from 'react';
 import WaveSurfer from 'wavesurfer.js';
 // import RecordPlugin from 'https://unpkg.com/wavesurfer.js@7/dist/plugins/record.esm.js'
-// import RecordPlugin from 'wavesurfer.js/dist/plugins/record.js'; // Assuming the plugin URL is this
-import RegionsPlugin from 'wavesurfer.js/dist/plugins/regions.js'; // Assuming the plugin URL is this
+// import RecordPlugin from 'wavesurfer.js/dist/plugins/record.js';
+import RegionsPlugin from 'wavesurfer.js/dist/plugins/regions.js';
 
 
-const AudioEditor = ({ audioUrl, tempFile }: { tempFile: boolean; audioUrl: string | null}) => {
+const AudioEditor = ({ audioFile }: { audioFile: File | null }) => {
     let index: number = 1;
+    if (audioFile) {
+        return (
+            <WaveAudio index={index} audioFile={audioFile} />
+        )
+    }
     return (
-        <WaveAudio index={index} pathToFile={audioUrl ? audioUrl : "./example.wav"} />
+        <p>
+            Error, no file sent
+        </p>
     )
 }
 
 let audioElements: any[] = [];
 
-function WaveAudio(props: { index: number; pathToFile: string}) {
+function WaveAudio(props: { index: number; audioFile: File }) {
     // const waveAudioRef = useRef<WaveSurfer>(null);
     const [isPlaying, setIsPlaying] = useState(false);
     const audioContainerRef = useRef<HTMLDivElement>(null);
@@ -27,24 +35,24 @@ function WaveAudio(props: { index: number; pathToFile: string}) {
         const audioElement = audioContainerRef.current;
         if (!audioElement) return;
         const waveform = WaveSurfer.create({
-                container: audioElement,
-                waveColor: "#363020",
-                progressColor: "#4F759B",
-                /** The color of the playpack cursor */
-                cursorColor: '#4F759B',
-                /** The cursor width */
-                cursorWidth: 2,
-                /** Play the audio on load */
-                autoplay: false,
-                /** Pass false to disable clicks on the waveform */
-                interact: true,
-                /** Render the waveform with bars like this: ▁ ▂ ▇ ▃ ▅ ▂ */
-                barWidth: 2,
-                /** Minimum pixels per second of audio (i.e. zoom level) */
-                minPxPerSec: 1,
-            });
+            container: audioElement,
+            waveColor: "#363020",
+            progressColor: "#4F759B",
+            /** The color of the playpack cursor */
+            cursorColor: '#4F759B',
+            /** The cursor width */
+            cursorWidth: 2,
+            /** Play the audio on load */
+            autoplay: false,
+            /** Pass false to disable clicks on the waveform */
+            interact: true,
+            /** Render the waveform with bars like this: ▁ ▂ ▇ ▃ ▅ ▂ */
+            barWidth: 2,
+            /** Minimum pixels per second of audio (i.e. zoom level) */
+            minPxPerSec: 1,
+        });
         // audioContainerRef.current = audioElement;
-        waveform.load(props.pathToFile);
+        waveform.load(props.audioFile.name);
         audioElements.push(waveform);
         // waveAudioRef.current = waveform;
 
@@ -59,15 +67,15 @@ function WaveAudio(props: { index: number; pathToFile: string}) {
             resize: false,
         })
 
-           // Handle region clicks
+        // Handle region clicks
         wsRegions.on('region-double-clicked', handleRegionClick);
 
         // Clean up the waveform instance on unmount
         return () => {
             waveform.destroy();
             audioElements = [];
-            };
-        }, [props.pathToFile]);
+        };
+    }, [props.audioFile]);
 
     const handleRegionClick = (region: any, event: any) => {
         console.log('Clicked region:', region);
@@ -80,38 +88,61 @@ function WaveAudio(props: { index: number; pathToFile: string}) {
         console.log('Toggled audio')
         const lastWaveformInstance = audioElements[audioElements.length - 1];
         if (lastWaveformInstance) {
-        if (isPlaying) {
-            lastWaveformInstance.pause();
-        } else {
-            lastWaveformInstance.play();
-        }
-        setIsPlaying((prevState) => !prevState);
+            if (isPlaying) {
+                lastWaveformInstance.pause();
+            } else {
+                lastWaveformInstance.play();
+            }
+            setIsPlaying((prevState) => !prevState);
         }
     };
 
-   return (
-    <div className="flex-col items-center space-y-2">
-      <div className="flex space-x-2">
-        <button
-          className="flex items-center justify-center w-12 h-12 rounded-full bg-blue-500 text-white"
-          onClick={handleWaveformClick}
-        >
-          {isPlaying ? 'Pause' : 'Play'}
-        </button>
-        <button
-          className="px-4 py-2 rounded-full bg-red-500 text-white"
-        //   onClick={handleBleep}
-        >
-          Bleep
-        </button>
-      </div>
-      <div
-        id={`waveform_${props.index}`}
-        // onClick={handleWaveformClick}
-        ref={audioContainerRef}
-      ></div>
-    </div>
-  ); 
+    const handleBleep = () => {
+        // Calls into POST
+        // Returns:
+        // Message: "status"
+        // "edited_audio": "base 64 encoded string",
+        // "proof": "base64 encoded string"
+
+        const formData = new FormData();
+        formData.append("file", props.audioFile);
+        formData.append("bucket_size", "123");
+        formData.append("left_indices", "1234 24 34 32")
+        formData.append("signature", "0x0000000000000000000000000000000000000000000000000000000000000000")
+        formData.append("bucket_datas", "")
+
+        axios.post('http://localhost:5000/api/audioUpload', formData)
+            .then((response) => {
+                console.log("Success", response)
+            })
+            .catch((error) => {
+                console.log("Error", error)
+            })
+    }
+
+    return (
+        <div className="flex-col items-center space-y-2">
+            <div className="flex space-x-2">
+                <button
+                    className="flex items-center justify-center w-12 h-12 rounded-full bg-blue-500 text-white"
+                    onClick={handleWaveformClick}
+                >
+                    {isPlaying ? 'Pause' : 'Play'}
+                </button>
+                <button
+                    className="px-4 py-2 rounded-full bg-red-500 text-white"
+                    onClick={handleBleep}
+                >
+                    Bleep
+                </button>
+            </div>
+            <div
+                id={`waveform_${props.index}`}
+                // onClick={handleWaveformClick}
+                ref={audioContainerRef}
+            ></div>
+        </div>
+    );
 }
 
 export default AudioEditor;
--- audio-player/src/pages/edit.tsx
+++ audio-player/src/pages/edit.tsx
@@ -19,42 +19,17 @@ const EditPage = () => {
     }
   }
 
-
-  /* Upload the file to the server */
-
+  /* Client side only solution */
   // const onFileUpload = (event: FormEvent<HTMLFormElement>) => {
   //   event.preventDefault()
-  //   // TODO: Save to `/public`
-  //   const url = 'http://localhost:5000/upload';
-  //   const formData = new FormData();
-  //   if (audioFile) {
-  //   formData.append('file', audioFile);
-  //   formData.append('fileName', audioFile.name);
+  //   if (event && event.target && event.target.files && event.target.files.length > 0) {
+  //     const file = event.target.files?.[0];
+  //     setAudioFile(file);
+  //     // const url = URL.createObjectURL(audioFile);
+  //     // console.log('NEW URL', url);
+  //     // setAudioUrl(file);
   //   }
-  //   const config = {
-  //     headers: {
-  //       'content-type': 'multipart/form-data',
-  //     },
-  //   };
-  //   console.log("FORM DATA", formData)
-  //   axios.post(url, formData, config).then((response) => {
-  //     console.log(response.data);
-  //   }).catch((error) => {
-  //     console.log(error);
-  //   });
   // }
-
-/* Client side only solution */
-  const onFileUpload = (event: FormEvent<HTMLFormElement>) => {
-    event.preventDefault()
-    //  const file = event.target.files?.[0];
-    if (audioFile) {
-      // setAudioFile(file);
-      const url = URL.createObjectURL(audioFile);
-      console.log('NEW URL', url);
-      setAudioUrl(url);
-    }
-  }
 
   const centeredDivStyle = {
     display: 'flex',
@@ -66,15 +41,20 @@ const EditPage = () => {
     <div>
       <Navbar />
       <div style={centeredDivStyle} className='h-64'></div>
-      <div className='w-full max-w-lg mx-auto'>
-      <  AudioEditor tempFile={audioFile === null}audioUrl={audioUrl} />
-      </div>
-      <div style={centeredDivStyle} className='py-10'>
-        <form onSubmit={onFileUpload}>
-          <input type="file" onChange={onFileChange} />
-          <button type="submit">Upload</button>
-        </form>
-      </div>
+      {audioFile &&
+        <div className='w-full max-w-lg mx-auto'>
+          <  AudioEditor audioFile={audioFile} />
+        </div>
+      }
+      {!audioFile &&
+        <div style={centeredDivStyle} className='py-10'>
+          <form>
+            {/* <form >onSubmit={onFileUpload}> */}
+            <input type="file" onChange={onFileChange} />
+            {/* <button type="submit">Upload</button> */}
+          </form>
+        </div>
+      }
     </div >
   );
 };
