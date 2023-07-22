1e2bd0b13b10f3116dd9eeb8651f90697697c424
Alex Cheema <alexcheema123@gmail.com>
2023-07-22 14:01:18 +0200

    separate audio_edit and generate scripts

--- scripts/audio_edit.js
+++ scripts/audio_edit.js
@@ -1,15 +1,4 @@
 const fs = require("fs");
-const keccak256 = require("keccak256");
-
-const N = 65536;
-
-function serialize_arr(arr) {
-    return "[" + arr.map(b => `"${b}"`).join(",") + "]";
-}
-
-function zero_pad(arr, num_zeroes) {
-    return arr.concat(new Array(num_zeroes).fill(0));
-}
 
 function zero_bytes(arr, start, end) {
     const res = arr.slice();
@@ -20,17 +9,8 @@ function zero_bytes(arr, start, end) {
 }
 
 async function editAudio() {
-    let wav = [...(await fs.promises.readFile("./test.wav"))];
-    const wav_hash = [...keccak256(wav)];
-    const zero_indices = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
-
-    const proverToml = 
-`wav = ${serialize_arr(wav)}
-wav_hash = ${serialize_arr(wav_hash)}
-zero_indices = ${serialize_arr(zero_indices)}`;
-
+    let wav = zero_bytes([...(await fs.promises.readFile("./test.wav"))], 2 * 3 * 1000, 2 * 3 * 2000);
     await fs.promises.writeFile("./out.wav", Buffer.from(wav));
-    await fs.promises.writeFile("./Prover.toml", proverToml);
 }
 
 editAudio().then(() => {
