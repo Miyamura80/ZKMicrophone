db999526203b337dc455a0a13f98ffc593e6c66b
Manuj Mishra <manujmishra2000@gmail.com>
2023-07-23 04:04:54 +0200

    lit attestation outline

--- audio-player/src/components/AudioCard.tsx
+++ audio-player/src/components/AudioCard.tsx
@@ -2,17 +2,15 @@ import React from 'react';
 import WaveAnimation from './WaveAnimation';
 
 interface AudioCardProps {
-  imageSrc: string;
   audioSrc: string;
-  username: string;
+  fileName: string;
 }
 
-const AudioCard: React.FC<AudioCardProps> = ({ imageSrc, audioSrc, username }) => {
+const AudioCard: React.FC<AudioCardProps> = ({audioSrc, fileName }) => {
   return (
     <div className="flex gap-x-4">
-      <img className="h-12 w-12 flex-none rounded-full bg-gray-50" src={imageSrc} alt="" />
       <div className="min-w-0 flex-auto">
-        <p className="text-sm font-semibold leading-6 text-gray-900">{username}</p>
+        <p className="text-sm font-semibold leading-6 text-gray-900">{fileName}</p>
         <p className="mt-1 truncate text-xs leading-5 text-gray-500">{audioSrc}</p>
       </div>
       <WaveAnimation />
