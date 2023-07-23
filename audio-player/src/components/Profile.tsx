b5974aa017d9d1dac96adcda472f3f67c252e895
lucasege <lucasege@cs.stanford.edu>
2023-07-23 08:11:33 +0200

    only MM

--- audio-player/src/components/Profile.tsx
+++ audio-player/src/components/Profile.tsx
@@ -4,23 +4,18 @@ export function Profile() {
     const { connect, connectors, error, pendingConnector } =
         useConnect()
 
+    console.log(connectors);
     return (
         <div>
-            {connectors.map((connector) => (
-                <button
-                    disabled={!connector.ready}
-                    key={connector.id}
-                    onClick={() => connector.connect()}
-                >
-                    {connector.name}
-                    {!connector.ready && ' (unsupported)'}
-                    {
-                        connector.id === pendingConnector?.id &&
-                        ' (connecting)'}
-                </button>
-            ))}
+            <button
+                disabled={false}
+                key={"metaMask"}
+                onClick={() => connectors[0].connect()}
+            >
+                {"MetaMask"}
+            </button>
 
             {error && <div>{error.message}</div>}
-        </div>
+        </div >
     )
 }
